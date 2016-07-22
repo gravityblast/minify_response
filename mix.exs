27a87d22547a2de0bcf0570e0174ac3f092eb131
Andrea Franz <andrea@gravityblast.com>
2016-07-22 17:12:18 +0200

    add package data

--- mix.exs
+++ mix.exs
@@ -7,7 +7,25 @@ defmodule MinifyResponse.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps()]
+  end
+
+  defp description do
+    """
+    A Plug that minifies response body
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Andrea Franz"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/pilu/minify_response",
+       "Docs" => "http://hexdocs.pm/minify_response/"}
+   ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +46,9 @@ defmodule MinifyResponse.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:plug, "~> 1.0"},
-     {:floki, "~> 0.9.0"}]
+     {:floki, "~> 0.9.0"},
+     {:ex_doc, "~> 0.11", only: :dev},
+     {:earmark, "~> 0.1", only: :dev},
+     {:dialyxir, "~> 0.3", only: :dev}]
   end
 end
